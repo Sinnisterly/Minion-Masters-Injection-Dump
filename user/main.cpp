5f3ea3626046c9f7a03c037b6ac089fd82fdd1fc
Sinnisterly <60723887+Sinnisterly@users.noreply.github.com>
2024-04-18 04:11:16 -0700

    Add project files.

---
+++ user/main.cpp
@@ -0,0 +1,16 @@
+#include "pch-il2cpp.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <iostream>
+#include "il2cpp-appdata.h"
+#include "helpers.h"
+
+using namespace app;
+
+void Run()
+{
+    il2cpp_thread_attach(il2cpp_domain_get());
+
+
+}
